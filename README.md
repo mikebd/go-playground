0dceb71b983ee93e11dd8568c3efffc73e49af3b
Michael Ben-David <michael.ben-david@applyboard.com>
2023-09-27 20:22:58 -0400

    Command line argument -lt
    Log with timestamps (UTC)

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # go-playground
 A playground for Golang experimentation
+
+## Running
+
+* `go run ./main <args>` - Run the main.go file with the given arguments
+
+## Arguments
+
+| Argument         | Description               |
+|------------------|---------------------------|
+| `-h` \| `--help` | Print the help message    |
+| `-lt`            | Log with timestamps (UTC) |
--- main/main.go
+++ main/main.go
@@ -1,9 +1,23 @@
 package main
 
-import "log"
+import (
+	"log"
+	"os"
+)
 
 func main() {
 	defer log.Println("Done")
 
+	args := parseArguments()
+	initializeLogging(args.logTimestamps)
 	log.Println("Starting")
+}
+
+func initializeLogging(logTimestamps bool) {
+	if logTimestamps {
+		log.SetFlags(log.LUTC | log.LstdFlags | log.Lshortfile)
+	} else {
+		log.SetFlags(log.Lshortfile)
+	}
+	log.SetOutput(os.Stdout)
 }
